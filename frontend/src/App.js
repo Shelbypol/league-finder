b0a5883191ba117fa2ae39235ecaa764c92b4034
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-20 15:33:20 -0600

    removed excess items/added home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
     return (
         <>
-          <h1>hello world</h1>
+            <HomeScreen/>
+
         </>
     );
 }
