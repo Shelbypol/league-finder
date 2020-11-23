9fa088323c0cad76c5058d5270b5539910d8c0b4
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-22 18:58:35 -0600

    bug found/ sever file renamed server

--- backend/server.js
+++ backend/server.js
@@ -32,5 +32,5 @@ app.use(errHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
