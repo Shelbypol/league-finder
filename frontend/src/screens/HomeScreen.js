d9acfa2c0cdab813605f975232d41310d21f3919
Shelby Polasek <shelbypolasek@gmail.com>
2020-11-24 20:04:21 -0600

    dynamic rendering of leagues table

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -67,7 +67,10 @@ const HomeScreen = ({history}) => {
             setSponsorReq(false)
         }
 
+        console.log(sponsorBudget);
         setAvailableLeagues([]);
+        setAvailable(true);
+
     };
 
     const submitSponsorReqHandler = () => {
@@ -79,16 +82,19 @@ const HomeScreen = ({history}) => {
 
     const budgetCalc = () => {
         let sponsorBudgetBucket = sponsorBudget;
+        // let sponsorLeaguesBucket = [];
 
         for (let i = 0; i <= leagues.length; i++) {
             leagues.map(league => {
                 if (((sponsorBudgetBucket - league.price) >= 0) && (!availableLeagues.includes(league.name))) {
                     sponsorBudgetBucket -= league.price;
-                    availableLeagues.push(league);
+                    // sponsorLeaguesBucket.push(league);
+                    availableLeagues.push(league)
                 }
             });
         }
-        setAvailable(!available)
+        // setAvailableLeagues(sponsorLeaguesBucket);
+        setAvailable(!available);
     };
 
 
@@ -96,7 +102,11 @@ const HomeScreen = ({history}) => {
         <>
             {/*======== ALL LEAGUES ========*/}
             <Row className='align-items-center'>
+                {available ?
                 <h3>All Leagues</h3>
+                :
+                    <h3>Available Leagues</h3>
+                }
                 <Col className='text-right'>
                     <Button className='my-3' type='button' onClick={createLeagueHandler}>
                         + Add League
@@ -124,7 +134,7 @@ const HomeScreen = ({history}) => {
                                 <th>LEAGUE NAME</th>
                                 <th>PRICE</th>
                                 <th>LOCATION</th>
-                                <th></th>
+                                <th> </th>
                             </tr>
                             </thead>
                             {available ? (
@@ -145,136 +155,127 @@ const HomeScreen = ({history}) => {
                                 </>
                             ) : (
                                 <>
-                                    {/* LEAGUES W/IN SPONSOR BUDGET */}
+                                    {/* LEAGUES W/IN SPONSOR BUDGET*/}
                                     <tbody>
                                     {availableLeagues.map((league => (
                                         <tr key={league._id}>
                                             <td>{league.name}</td>
                                             <td>${league.price}</td>
                                             <td>{league.location.city}, {league.location.state}</td>
-                                            <td>
-                                                <Button variant='danger' className='btn-sm'
-                                                        onClick={() => deleteHandler(league._id)}> x </Button>
-                                            </td>
                                         </tr>
                                     )))}
                                     </tbody>
                                 </>
-
-                            )
-                            }
-                                < /Table>
-
-                                )
-                            }
-
-
-                            {/*======== SPONSOR FORM ========*/}
-                            {sponsorBtn && (
-                                <Col>
-                                    <>
-                                        <Form onSubmit={submitSponsorReqHandler}>
-                                            <h4>Enter Search Requirements</h4>
-                                            <i>( search for leagues within your budget and search radius )</i>
-                                            {/* BUDGET */}
-                                            <Form.Group controlId='budget'>
-                                                <Form.Label><strong>Budget</strong></Form.Label>
-                                                <Form.Control type='budget'
-                                                              placeholder='Enter budget'
-                                                              onChange={(e) => setSponsorBudget(e.target.value)}
-                                                >
-                                                </Form.Control>
-                                            </Form.Group>
-
-                                            {/* RADIUS */}
-                                            <Form.Group controlId='radius'>
-                                                <Form.Label><strong>Search Radius</strong><i>( in miles
-                                                    )</i></Form.Label>
-                                                <Form.Control type='radius'
-                                                              placeholder='Enter search radius'
-                                                              onChange={(e) => setSponsorRadius(e.target.value)}
-                                                >
-                                                </Form.Control>
-                                            </Form.Group>
-
-                                            {/* ADDRESS */}
-                                            <Form.Group controlId='address'>
-                                                <Form.Label><strong>Street Address</strong></Form.Label>
-                                                <Form.Control type='text'
-                                                              placeholder='Enter street address'
-                                                              onChange={(e) => setSponsorAddress(e.target.value)}
-                                                >
-                                                </Form.Control>
-                                            </Form.Group>
-                                            {/* CITY */}
-                                            <Form.Group controlId='city'>
-                                                <Form.Label><strong>City</strong></Form.Label>
-                                                <Form.Control type='text'
-                                                              placeholder='Enter city'
-                                                              onChange={(e) => setSponsorCity(e.target.value)}
-                                                >
-                                                </Form.Control>
-                                            </Form.Group>
-                                            {/* STATE */}
-                                            <Form.Group controlId='state'>
-                                                <Form.Label><strong>State</strong></Form.Label>
-                                                <Form.Control type='text'
-                                                              placeholder='Enter state'
-                                                              onChange={(e) => setSponsorState(e.target.value)}
-                                                >
-                                                </Form.Control>
-                                            </Form.Group>
-                                            {/* POSTAL CODE */}
-                                            <Form.Group controlId='postalCode'>
-                                                <Form.Label><strong>Zip Code</strong></Form.Label>
-                                                <Form.Control type='text'
-                                                              placeholder='Enter zip code'
-                                                              onChange={(e) => setSponsorPostal(e.target.value)}
-                                                >
-                                                </Form.Control>
-                                            </Form.Group>
-                                            {/* COUNTRY */}
-                                            <Form.Group controlId='country'>
-                                                <Form.Label><strong>Country</strong></Form.Label>
-                                                <Form.Control type='text'
-                                                              placeholder='Enter country'
-                                                              onChange={(e) => setSponsorCountry(e.target.value)}
-                                                >
-                                                </Form.Control>
-                                            </Form.Group>
-
-
-                                            <Button type='submit' variant='primary'>Search Leagues</Button>
-                                        </Form>
-                                    </>
-                                </Col>
                             )}
-
-                            {/*======== SPONSOR SEARCH REQUIREMENTS ========*/}
-                            {sponsorReq ? (
-                                <>
-                                    <h5>Sponsor Search Requirements</h5>
-                                    <Table striped bordered hover responsive className='table-sm mb-5'>
-                                        <thead>
-                                        <tr>
-                                            <th>BUDGET</th>
-                                            <th>RADIUS</th>
-                                            <th>STARTING LOCATION</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <tr>
-                                            <td>${sponsorBudget}</td>
-                                            <td>{sponsorRadius} miles</td>
-                                            <td>{sponsorAddress}, {sponsorState}</td>
-                                        </tr>
-                                        </tbody>
-                                    </Table>
-                                </>
-                            ) : ('')}
-
-                        </>
+                        </Table>
                     )
-            };
+            }
+            {/*======== SPONSOR FORM ========*/}
+            {sponsorBtn && (
+                <Col>
+                    <>
+                        <Form onSubmit={submitSponsorReqHandler}>
+                            <h4>Enter Search Requirements</h4>
+                            <i>( search for leagues within your budget and search radius )</i>
+                            {/* BUDGET */}
+                            <Form.Group controlId='budget'>
+                                <Form.Label><strong>Budget</strong></Form.Label>
+                                <Form.Control type='budget'
+                                              placeholder='Enter budget'
+                                              onChange={(e) => setSponsorBudget(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
 
-            export default HomeScreen
+                            {/* RADIUS */}
+                            <Form.Group controlId='radius'>
+                                <Form.Label><strong>Search Radius</strong><i>( in miles
+                                    )</i></Form.Label>
+                                <Form.Control type='radius'
+                                              placeholder='Enter search radius'
+                                              onChange={(e) => setSponsorRadius(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+
+                            {/* ADDRESS */}
+                            <Form.Group controlId='address'>
+                                <Form.Label><strong>Street Address</strong></Form.Label>
+                                <Form.Control type='text'
+                                              placeholder='Enter street address'
+                                              onChange={(e) => setSponsorAddress(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+                            {/* CITY */}
+                            <Form.Group controlId='city'>
+                                <Form.Label><strong>City</strong></Form.Label>
+                                <Form.Control type='text'
+                                              placeholder='Enter city'
+                                              onChange={(e) => setSponsorCity(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+                            {/* STATE */}
+                            <Form.Group controlId='state'>
+                                <Form.Label><strong>State</strong></Form.Label>
+                                <Form.Control type='text'
+                                              placeholder='Enter state'
+                                              onChange={(e) => setSponsorState(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+                            {/* POSTAL CODE */}
+                            <Form.Group controlId='postalCode'>
+                                <Form.Label><strong>Zip Code</strong></Form.Label>
+                                <Form.Control type='text'
+                                              placeholder='Enter zip code'
+                                              onChange={(e) => setSponsorPostal(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+                            {/* COUNTRY */}
+                            <Form.Group controlId='country'>
+                                <Form.Label><strong>Country</strong></Form.Label>
+                                <Form.Control type='text'
+                                              placeholder='Enter country'
+                                              onChange={(e) => setSponsorCountry(e.target.value)}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+
+
+                            <Button type='submit' variant='primary'>Search Leagues</Button>
+                        </Form>
+                    </>
+                </Col>
+            )}
+
+            {/*======== SPONSOR SEARCH REQUIREMENTS ========*/}
+            {sponsorReq ? (
+                <>
+                    <h5>Sponsor Search Requirements</h5>
+                    <Table striped bordered hover responsive className='table-sm mb-5'>
+                        <thead>
+                        <tr>
+                            <th>BUDGET</th>
+                            <th>RADIUS</th>
+                            <th>STARTING LOCATION</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>${sponsorBudget}</td>
+                            <td>{sponsorRadius} miles</td>
+                            <td>{sponsorAddress}, {sponsorState}</td>
+                        </tr>
+                        </tbody>
+                    </Table>
+                </>
+            ) : ('')
+            }
+        </>
+    )
+};
+
+    export default HomeScreen
